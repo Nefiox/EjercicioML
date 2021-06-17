950d098b2ee90080415544703708b344b3103d08
Juan Jiménez <nefioxdev@gmail.com>
2021-06-17 18:51:52 -0500

    Login listo

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // OPCIÓN 1: USANDO PATH Y REFERENCIANDO
 // const path = require('path');
 // const publicPath = path.join(__dirname, '/public')
@@ -10,15 +13,15 @@ const PORT = 3000;
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/views/home.html'))
 // });
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 // OPCIÓN 2: SIN USAR PATH
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.sendFile('/home.html', {root: 'views'});
-    
-});
+app.get('/', (req, res) => res.sendFile('/home.html', {root: 'views'}));
 app.get('/registro', (req, res) => res.sendFile('/register.html', {root: 'views'}));
+app.get('/login', (req, res) => res.sendFile('/login.html', {root: 'views'}));
 
 
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
